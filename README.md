f41587ea1f54886137585a2880d823bab4daa935
ShibainuTen <59001330+ShibainuTen@users.noreply.github.com>
2020-02-05 14:51:20 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# BOTっぽい何か ver.1.0.2
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE) [![Maintainability](https://api.codeclimate.com/v1/badges/6b6ac25e1d6752fe67ae/maintainability)](https://codeclimate.com/github/massongit/LINE-BOT/maintainability)  
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## 概要
+[Heroku](https://www.heroku.com/)で動かすことを想定したLINE BOTです。
+
+## 開発環境
+Python 3.7
+
+## 使用サービス
+* [Heroku](https://www.heroku.com/)
+* [LINE Messaging API](https://developers.line.me/ja/services/messaging-api/)
+
+## 機能
+* 暴言が来たら、『なんだと(# ﾟДﾟ)』と返す
+* 『{1文字以上の文字列}っぽー』と来たら、『ほっほー(・∀・)』と返す
+* 上記2つに当てはまらない文字列が来たら、文字列をひらがなに変換し、単語ごとに区切った上で返す
+
+## Webhook URL ([LINE Messaging API](https://developers.line.me/ja/services/messaging-api/))
+https://{アプリ名}.herokuapp.com/callback
+
+## Config Variables ([Heroku](https://www.heroku.com/))
+|||
+|:--:|:--:|
+|`LINE_CHANNEL_SECRET`|Channel Secret ([LINE Messaging API](https://developers.line.me/ja/services/messaging-api/))|
+|`LINE_CHANNEL_ACCESS_TOKEN`|アクセストークン ([LINE Messaging API](https://developers.line.me/ja/services/messaging-api/))|
+|`DEBUG`|デバッグするかどうか (`True`, `False` / デフォルト値: `False`)|
---
+++ bot.py
@@ -0,0 +1,132 @@
+from flask import Flask, request, abort
+from pathlib import Path
+import linebot
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import ImageMessage, MessageEvent, TextMessage, TextSendMessage
+import base64
+import requests, json, os, io
+from io import BytesIO
+from PIL import Image
+import numpy as np
+from keras.preprocessing.image import img_to_array, load_img
+from keras.models import load_model
+import gc
+
+print('***ライブラリのインポート***')
+
+app = Flask(__name__)
+
+line_bot_api = linebot.LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
+handler = linebot.WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
+
+
+header = {
+    "Content-Type": "application/json",
+    "Authorization":  "Bearer " + os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+}
+
+# model はグローバルで宣言し、初期化しておく
+model = None
+
+print('****前準備****')
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+    print('****コールバック****')
+
+# テキストを受け取る部分
+@handler.add(MessageEvent, message=TextMessage)
+def handler_message(event):
+     print("****handle_message****:", event)
+     line_bot_api.reply_message(event.reply_token,TextSendMessage('コーギー、パグ、柴犬、キャバリア、チワワ、ダックス、プードル'))
+
+# 画像を受け取る部分
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    print("****handle_image****:", event)
+    
+    message_id = event.message.id
+    
+    # message_idから画像のバイナリデータを取得
+    message_content = line_bot_api.get_message_content(message_id)
+    SRC_IMAGE_PATH = "static/images/{}.jpg"
+    save_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+
+    with open(save_path, "wb") as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+    
+    image_text = get_text_by_ms(event,save_path)
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=image_text))
+    
+def get_text_by_ms(event,result):
+
+    # 90行目で保存した url から画像を書き出す。
+    img = img_to_array(load_img(result, target_size=(256,256)))
+    face=""
+    # グローバル変数を取得する
+    global model
+
+    # model をロードする
+    model = load_model('./acc_77-.h5')
+    
+    # 0-1に変換
+    img_nad = (img_to_array(img)/255)
+
+    # 4次元配列に
+    img_nad = img_nad[None, ...]
+    
+    predict = model.predict(img_nad)
+    print('***predict***',predict)
+    faceNumLabel=np.argmax(predict)
+    score = np.max(predict)
+    text = detect_who(faceNumLabel)
+    score = '{:.1%}'.format(score)
+    text = text + ':' + score   
+            
+    print('***text***',text)
+    
+    del img_nad,img,model
+    gc.collect()
+    
+    return text
+
+def detect_who(faceNumLabel):
+   
+    # 「判定」
+    if faceNumLabel == 0:
+        face = "コーギー"
+    elif faceNumLabel == 1:
+        face = "柴犬"
+    elif faceNumLabel == 2:
+        face = "パグ"
+    elif faceNumLabel == 3:
+        face = "キャバリア"
+    elif faceNumLabel == 4:
+        face = "チワワ"
+    elif faceNumLabel == 5:
+        face = "ダックスフンド"
+    elif faceNumLabel == 6:
+        face = "プードル" 
+    
+    print('****face****',face)
+    return face
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
